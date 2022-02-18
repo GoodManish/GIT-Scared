855ba90b076af87dd34da320cee14caecc2989cf
Manish <van.manish@gmail.com>
2022-02-18 19:16:24 +0530

    testing Login feature

--- src/test/java/com/example/demo/DevTestClass.java
+++ src/test/java/com/example/demo/DevTestClass.java
@@ -4,5 +4,6 @@ public class DevTestClass {
 
     public static void main(String[] args) {
         System.out.println("In Junit -- test class");
+        System.out.println("-- Testing Login feature ---");
     }
 }
