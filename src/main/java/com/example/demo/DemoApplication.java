ff2b0e1767038b7cdb080b332957907a6b9480d6
GoodManish2 <100021337+GoodManish2@users.noreply.github.com>
2022-02-19 15:41:06 +0530

    Logging added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("--- Feature worked by goodmanish2 ---");  //GoodManish2 commit
+		System.out.println("--- Feature Logging updated ---");
 	}
 
 }
