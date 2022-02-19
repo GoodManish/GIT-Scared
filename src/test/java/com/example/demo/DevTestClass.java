87ab559e02ad001c124a2412d7c19191aca06149
Manish <van.manish@gmail.com>
2022-02-19 16:24:16 +0530

    integration e2e feature added

--- src/test/java/com/example/demo/DevTestClass.java
+++ src/test/java/com/example/demo/DevTestClass.java
@@ -5,5 +5,10 @@ public class DevTestClass {
     public static void main(String[] args) {
         System.out.println("In Junit -- test class");
         System.out.println("-- Testing Login feature ---");
+
+        System.out.println("-- My in-progress Changes --");
+        System.out.println("API call");
+        System.out.println("Event call");
+        System.out.println("DB call");
     }
 }
